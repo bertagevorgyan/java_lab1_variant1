bf6592aa346b96a7b7990d868b1201c998ba2e29
bertagevorgyan <gberta@vk.com>
2025-10-23 17:11:02 +0300

    Update Main.java

--- to-do list/Main.java
+++ to-do list/Main.java
@@ -1,13 +1,13 @@
-import javax.swing.*;
+import javax.swing.*;  // библиотека swing для создания графического интерфейса
 
 public class TodoApp {
   public static void main(String[] args) {
     try {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     } catch (Exception e) {
-      e.printStackTrace();
+      e.printStackTrace();   //для интерфейса программы устанавливается тот же стиль, что и у ОС 
     }
 
-    SwingUtilities.invokeLater(() -> new TodoFrame(new TaskManager()));
+    SwingUtilities.invokeLater(() -> new TodoFrame(new TaskManager())); //создается стартовое окно и ему передаётся объект TaskManager
   }
 }
